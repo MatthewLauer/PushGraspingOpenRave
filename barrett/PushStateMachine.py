c7f8dca6e624b999061acef9433a678c1ac0e3df
bb8 <mlauer591@gmail.com>
2017-11-26 15:45:45 -0500

    can now check whether standard technique can find grasp

--- barrett/PushStateMachine.py
+++ barrett/PushStateMachine.py
@@ -148,97 +148,112 @@ def loadGoalsInEnvironment(PSM, env, sigma, goalPos, goalRadius, numGoalSamples)
             env.AddKinBody(goal[i])
     return (goal, goaltrans)
 
+def standardGraspAttempt(self, goal):
+	gmodel = openravepy.databases.grasping.GraspingModel(robot, goal[0])
+	if not gmodel.load():
+		gmodel.autogenerate()
+
+	validgrasps, validindices = gmodel.computeValidGrasps(returnnum=10)
+	if(validgrasps is None):
+		return False
+	validgrasp = validgrasps[0]
+	Tgrasp = gmodel.getGlobalGraspTransform(validgrasp, collisionfree=True)
+	self.basemanip.MoveToHandPosition(matrices=[Tgrasp])
+	#robot.WaitForController(0)
+	return True
+
+
 if __name__ == "__main__":
-    env = openravepy.Environment()
-    env.Load('env.xml')
-    env.SetViewer('qtcoin')
-    PSM = PushStateMachine(env)
-    goalRadius = .05;
-    sigma = .02
-    CR = CaptureRegion(goalRadius/2)
-    #More Initialization                            +
-    bodySampleSize = 1
-    numObstacles = 3
-    obstacleSizes = [(0.05, 0.1, 0.1),(0.05, 0.1, 0.1),(0.05, 0.1, 0.1)]
-    numObstacleSamples = bodySampleSize
-    numGoalSamples = bodySampleSize
+	env = openravepy.Environment()
+	env.Load('env.xml')
+	env.SetViewer('qtcoin')
+	PSM = PushStateMachine(env)
+	goalRadius = .05;
+	sigma = .02
+	CR = CaptureRegion(goalRadius/2)
+	#More Initialization                            +
+	bodySampleSize = 1
+	numObstacles = 3
+	obstacleSizes = [(0.05, 0.1, 0.1),(0.05, 0.1, 0.1),(0.05, 0.1, 0.1)]
+	numObstacleSamples = bodySampleSize
+	numGoalSamples = bodySampleSize
 
-    goalPos = (-0.7, -0.37, 1.112)
-    maxDist = 0.3
+	goalPos = (-0.7, -0.37, 1.112)
+	maxDist = 0.3
 
-    obstacles = loadObstaclesInEnvironment(PSM, env, sigma, goalPos, goalRadius, maxDist, numObstacles, obstacleSizes, numObstacleSamples)
-    (goal, goaltrans) = loadGoalsInEnvironment(PSM, env, sigma, goalPos, goalRadius, numGoalSamples)
+	obstacles = loadObstaclesInEnvironment(PSM, env, sigma, goalPos, goalRadius, maxDist, numObstacles, obstacleSizes, numObstacleSamples)
+	(goal, goaltrans) = loadGoalsInEnvironment(PSM, env, sigma, goalPos, goalRadius, numGoalSamples)
 
-    viewer = env.GetViewer()
-    viewer.SetBkgndColor([.8, .85, .9])  # RGB tuple
-    robot = env.GetRobots()[0]
-    dofs = [-2.0,1.0,2.0,-.5,-1.0,1.0,1.0,1.0,1.0,1.0,0.0]
-    robot.SetActiveDOFValues(dofs)
+	viewer = env.GetViewer()
+	viewer.SetBkgndColor([.8, .85, .9])  # RGB tuple
+	robot = env.GetRobots()[0]
+	dofs = [-2.0,1.0,2.0,-.5,-1.0,1.0,1.0,1.0,1.0,1.0,0.0]
+	robot.SetActiveDOFValues(dofs)
 
-    #Setup parameters for offsets to loop through
-    minAperture = CR.minAperture
-    vStepCount=15; oStepCount=1; aStepCount = 4; backwardincrement = .03
+	#Setup parameters for offsets to loop through
+	minAperture = CR.minAperture
+	vStepCount=15; oStepCount=1; aStepCount = 4; backwardincrement = .03
 
-    trans = goal[0].GetTransform()
-    sol = numpy.array([0,0,0,0,0,0,0])#7Dof #-(CR.angleToFullHandWidth(0)-CR.angleToFullHandWidth(minAperture))/2
-    oSteps = numpy.linspace(0,
-        (CR.angleToFullHandWidth(0)-CR.angleToFullHandWidth(minAperture))/2, num=oStepCount*2+1)
-    vSteps = numpy.linspace(0,2*math.pi, num = vStepCount, endpoint = False)
-    aSteps = numpy.linspace(0,minAperture,num = aStepCount)
+	trans = goal[0].GetTransform()
+	sol = numpy.array([0,0,0,0,0,0,0])#7Dof #-(CR.angleToFullHandWidth(0)-CR.angleToFullHandWidth(minAperture))/2
+	oSteps = numpy.linspace(0,
+	    (CR.angleToFullHandWidth(0)-CR.angleToFullHandWidth(minAperture))/2, num=oStepCount*2+1)
+	vSteps = numpy.linspace(0,2*math.pi, num = vStepCount, endpoint = False)
+	aSteps = numpy.linspace(0,minAperture,num = aStepCount)
 
-    CR.initializeCaptureRegions(aSteps)
-    #Loop through offset parameters a,v,o   
-    #time.sleep(5)
-    data_file = open("Data.txt", "a")
-    data_file.write("\nSamples: %f Sigma: %f Obstacles: %f" %(bodySampleSize, sigma, numObstacles))
+	CR.initializeCaptureRegions(aSteps)
+	#Loop through offset parameters a,v,o   
+	#time.sleep(5)
+	data_file = open("Data.txt", "a")
+	data_file.write("\nSamples: %f Sigma: %f Obstacles: %f" %(bodySampleSize, sigma, numObstacles))
 
-    push = None
-    start = time.time()
-    for o in oSteps:
-        for v in vSteps:
-            for a in aSteps:
-                pose = PSM.GetPose(o,v,a,goaltrans) 
-                if(pose is None):
-                    continue
+	push = None
+	start = time.time()
+	for o in oSteps:
+	    for v in vSteps:
+	        for a in aSteps:
+	            pose = PSM.GetPose(o,v,a,goaltrans) 
+	            if(pose is None):
+	                continue
 
-                #print pose
-                #print 'o: %f v: %f a: %f' % (o,v,a)
-                robot.SetActiveDOFValues(pose)
-                robottrans = robot.GetManipulator("arm").GetTransform()
-                p = (robottrans[1][3], robottrans[0][3], v)
-                #print "Hand Pose:"
-                #print p
-                #print "Goals Samples:"
-                for goalSample in goal:
-                    goalSampleTrans = goalSample.GetTransform()
-                    gSamples = (goalSampleTrans[1][3], goalSampleTrans[0][3], 1)
-                    #print gSamples
-                    success = CR.isInCaptureRegion(p, a, gSamples)
-                    if success[0] == False:
-                        break
+	            #print pose
+	            #print 'o: %f v: %f a: %f' % (o,v,a)
+	            robot.SetActiveDOFValues(pose)
+	            robottrans = robot.GetManipulator("arm").GetTransform()
+	            p = (robottrans[1][3], robottrans[0][3], v)
+	            #print "Hand Pose:"
+	            #print p
+	            #print "Goals Samples:"
+	            for goalSample in goal:
+	                goalSampleTrans = goalSample.GetTransform()
+	                gSamples = (goalSampleTrans[1][3], goalSampleTrans[0][3], 1)
+	                #print gSamples
+	                success = CR.isInCaptureRegion(p, a, gSamples)
+	                if success[0] == False:
+	                    break
 
-                if(success[0] == False):
-                    continue
-                dofs[7:10] = [a,a,a]
-                robot.SetActiveDOFValues(dofs)
-                PSM.MoveGripper(robottrans)
-                for goalSample in goal:
-                    env.Remove(goalSample)
-                push = PSM.MoveGripperStraight(distance= success[1], Tee = robottrans, dirAngle= v)
-                for goalSample in goal:
-                    env.AddKinBody(goalSample)
-                if push is not None:
-                    print "Push Grasp Found"
-                    data_file.write("\n%f" %(time.time()-start))
-                    start = time.time()
-                    break
-                    #time.sleep(1)
-            if push is not None:
-                break
-        if push is not None:
-            break
-    data_file.close()
-    #IPython.embed() 
+	            if(success[0] == False):
+	                continue
+	            dofs[7:10] = [a,a,a]
+	            robot.SetActiveDOFValues(dofs)
+	            PSM.MoveGripper(robottrans)
+	            for goalSample in goal:
+	                env.Remove(goalSample)
+	            push = PSM.MoveGripperStraight(distance= success[1], Tee = robottrans, dirAngle= v)
+	            for goalSample in goal:
+	                env.AddKinBody(goalSample)
+	            if push is not None:
+	                print "Push Grasp Found"
+	                data_file.write("\n%f" %(time.time()-start))
+	                start = time.time()
+	                break
+	                #time.sleep(1)
+	        if push is not None:
+	            break
+	    if push is not None:
+	        break
+	data_file.close()
+	#IPython.embed() 
 
 
     #   GetPose                                     +
